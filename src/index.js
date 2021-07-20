53ee93ef350e22619326616eea6015896da57efb
Dror Ben-Gai <dror@spectralops.io>
2021-07-20 12:12:19 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+
+async function bootstrap() {
+  throw new Error('ouch!')
+}
+
+bootstrap().then(initVals => {
+  console.log('>> Insecurity in progress - what could go wrong?', initVals)
+}).catch(err => {
+  console.error('Oh no! something bad happened..')
+})
